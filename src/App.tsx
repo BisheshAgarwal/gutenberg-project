40c2504866e85b748f7cb8a5ec06561c3e6f0a0c
Bishesh Agarwal <bisheshagarwal1999@gmail.com>
2025-03-22 10:45:01 +0530

    Add the search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
+import SearchInput from "./components/custom/search";
+
 function App() {
-  return <h1>Hello world</h1>;
+  const [value, setValue] = useState("");
+  return (
+    <div className="w-[300px] ml-10 mt-5">
+      <SearchInput value={value} onChange={(value) => setValue(value)} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/custom/search.tsx
@@ -0,0 +1,36 @@
+import { Search, X } from "lucide-react";
+import { Input } from "../ui/input";
+import { cn } from "@/lib/utils";
+
+interface SearchInputProps {
+  value: string;
+  onChange: (e: string) => void;
+}
+
+const SearchInput = ({ value, onChange }: SearchInputProps) => {
+  return (
+    <div
+      className={cn(
+        "rounded-sm h-[40px] px-[10px] flex items-center bg-grey-primary focus-within:ring-1 focus-within:ring-primary transition-all",
+        value && "ring-1 ring-primary"
+      )}
+    >
+      <Search size={18} className="text-grey-secondary" />
+      <Input
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        className="border-none shadow-none focus-visible:ring-0 focus:ring-0 focus:outline-none w-full pl-2 placeholder:text-grey-secondary placeholder:font-medium"
+        placeholder="Search"
+      />
+      {value && (
+        <X
+          size={22}
+          className="cursor-pointer text-grey-secondary"
+          onClick={() => onChange("")}
+        />
+      )}
+    </div>
+  );
+};
+
+export default SearchInput;
