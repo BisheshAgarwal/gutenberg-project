8138cd331d34274de8f268dfb51132f53e0f1281
Bishesh Agarwal <bisheshagarwal1999@gmail.com>
2025-03-22 11:00:11 +0530

    Create the genre card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import SearchInput from "./components/custom/search";
+import GenreCard from "./components/custom/genre-card";
 
 function App() {
   const [value, setValue] = useState("");
   return (
-    <div className="w-[300px] ml-10 mt-5">
+    <div className="w-[300px] ml-10 mt-5 flex flex-col gap-5">
       <SearchInput value={value} onChange={(value) => setValue(value)} />
+      <GenreCard genre="GENRE" />
     </div>
   );
 }
---
+++ src/components/custom/genre-card.tsx
@@ -0,0 +1,18 @@
+interface GenreCardProps {
+  genre: string;
+}
+
+import Fiction from "@/assets/Fiction.svg";
+import Next from "@/assets/Next.svg";
+
+const GenreCard = ({ genre = "" }: GenreCardProps) => {
+  return (
+    <div className="bg-white rounded-sm px-[10px] h-[50px] shadow-[0_2px_5px_0_rgba(211,209,238,0.5)] flex items-center">
+      <img src={Fiction} alt="Fiction Icon" className="w-6 h-6 mr-2" />
+      <p className="font-semibold text-xl">{genre.toUpperCase()}</p>
+      <img src={Next} alt="Next Icon" className="w-4 h-4 block ml-auto" />
+    </div>
+  );
+};
+
+export default GenreCard;
